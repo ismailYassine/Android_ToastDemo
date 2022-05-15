8906fb3a687d02c9e5bbbfa10d6e223c9fd2a3f6
Yassine Ismail <yassine.iguire@gmail.com>
2022-05-15 10:55:41 -0400

    Initial commit

---
+++ app/src/main/java/com/example/androidtoastdemo/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.androidtoastdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnShowToast;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnShowToast = findViewById(R.id.btnShowToast);
+        btnShowToast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(getApplicationContext(), "This is a Toast", Toast.LENGTH_LONG);
+                toast.show();
+
+            }
+        });
+    }
+}
